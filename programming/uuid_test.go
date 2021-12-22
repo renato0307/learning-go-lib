5cee154f5b60862afb8a253b4cf9fd07268a4426
Renato Torres <renato.torres@outsystems.com>
2021-12-22 20:08:55 +0000

    refactor: introduced interfaces and mocks

--- programming/uuid_test.go
+++ programming/uuid_test.go
@@ -6,15 +6,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var pf ProgrammingFunctions = ProgrammingFunctions{}
+
 func TestNewUuidWithHyphen(t *testing.T) {
-	uuidWithHyphen := NewUuid(false)
+	uuidWithHyphen := pf.NewUuid(false)
 
 	assert.Len(t, uuidWithHyphen, 36)
 	assert.Contains(t, uuidWithHyphen, "-")
 }
 
 func TestNewUuidWithoutHyphen(t *testing.T) {
-	uuidWithHyphen := NewUuid(true)
+	uuidWithHyphen := pf.NewUuid(true)
 
 	assert.Len(t, uuidWithHyphen, 32)
 	assert.NotContains(t, uuidWithHyphen, "-")
