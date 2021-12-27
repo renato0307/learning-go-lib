7a1ebbc49fc1fad572f9b9d82bba3139b1e20a36
Renato Torres <renato.torres@outsystems.com>
2021-12-27 23:39:01 +0000

    feat: add currency converter

---
+++ finance/interface.go
@@ -0,0 +1,13 @@
+package finance
+
+type Interface interface {
+	ConvertCurrency(
+		from string,
+		to string,
+		amount float64) (float64, error)
+}
+
+type FinanceFunctions struct {
+	ApiUrl string
+	ApiKey string
+}
